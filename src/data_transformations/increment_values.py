6aa3e1071aa143e0a2a24838f0baac49d62fcae0
svbogdanov <sb104@yandex.ru>
2022-12-15 14:40:35 +0100

    data pipeline implemented

---
+++ src/data_transformations/increment_values.py
@@ -0,0 +1,13 @@
+import sys
+
+import pandas as pd
+
+
+def main() -> None:
+    df = pd.read_csv("data/dvc_practice_data/initial.csv")
+    df += 1
+    df.to_csv("data/dvc_practice_data/transformed.csv", index=False)
+
+
+if __name__ == "__main__":
+    main()
